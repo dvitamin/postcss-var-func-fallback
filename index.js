4c09a8068d6416c6a2faa71c06ed60f0f5f68f6e
David Sveningsson <ext@sidvind.com>
2022-10-26 01:19:48 +0200

    feat!: require postcss v8

--- index.js
+++ index.js
@@ -1,62 +1,78 @@
-'use strict';
+"use strict";
 
-const postcss = require('postcss');
-const parser = require('postcss-value-parser');
-const processed = Symbol('processed');
+const parser = require("postcss-value-parser");
+const processed = Symbol("processed");
 
 const defaults = {
   variables: {},
   treatErrorsAsWarnings: false,
-}
+};
 
-module.exports = postcss.plugin('postcss-var-func-fallback', (options) => {
-  const opts = {...defaults, ...options};
-  return (root, result) => {
-    root.walkDecls(decl => {
-
-      function failOrWarn(result, text) {
-        if(opts.treatErrorsAsWarnings) {
-          decl.warn(result, text);
-          return;
+const plugin = (options = {}) => {
+  const opts = { ...defaults, ...options };
+  return {
+    postcssPlugin: "postcss-var-func-fallback",
+    Once(root, { result }) {
+      root.walkDecls((decl) => {
+        function failOrWarn(result, text) {
+          if (opts.treatErrorsAsWarnings) {
+            decl.warn(result, text);
+            return;
+          }
+          throw Error(text);
         }
-        throw Error(text);
-      }
 
-      if (decl[processed] || !decl.value.includes('var(--')) {
-        return;
-      }
-
-      let updateDecl = false;
-      const parsedValue = parser(decl.value);
-
-      parsedValue.walk((node) => {
-        if (node.type !== 'function' || node.value !== 'var' || !node.nodes || !node.nodes.length) {
+        if (decl[processed] || !decl.value.includes("var(--")) {
           return;
         }
 
-        const varName = node.nodes[0].value;
+        let updateDecl = false;
+        const parsedValue = parser(decl.value);
 
-        if (node.nodes.length > 1) {
-          failOrWarn(result, `Fallback value already provided for variable ${varName}`)
-          return;
+        parsedValue.walk((node) => {
+          if (
+            node.type !== "function" ||
+            node.value !== "var" ||
+            !node.nodes ||
+            !node.nodes.length
+          ) {
+            return;
+          }
+
+          const varName = node.nodes[0].value;
+
+          if (node.nodes.length > 1) {
+            failOrWarn(
+              result,
+              `Fallback value already provided for variable ${varName}`
+            );
+            return;
+          }
+
+          const varValue = opts.variables[varName];
+
+          if (!varValue) {
+            failOrWarn(
+              result,
+              `Fallback value not found for variable ${varName}`
+            );
+            return;
+          }
+
+          node.nodes.push({ type: "div", value: ",", before: "", after: " " });
+          node.nodes.push({ type: "word", value: varValue });
+          updateDecl = true;
+        });
+
+        if (updateDecl) {
+          decl.value = parsedValue.toString();
+          decl[processed] = true;
         }
-
-        const varValue = opts.variables[varName];
-
-        if (!varValue) {
-          failOrWarn(result, `Fallback value not found for variable ${varName}`)
-          return;
-        }
-
-        node.nodes.push({ type: 'div', value: ',', before: '', after: ' ' });
-        node.nodes.push({ type: 'word', value: varValue });
-        updateDecl = true;
       });
+    },
+  };
+};
 
-      if (updateDecl) {
-        decl.value = parsedValue.toString();
-        decl[processed] = true;
-      }
-    });
-  }
-});
+plugin.postcss = true;
+
+module.exports = plugin;
